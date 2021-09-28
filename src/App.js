d23d6571096fab0299435cace3dad25b86248433
ridwanmalik <skredonemalik@gmail.com>
2021-09-28 06:21:44 +0600

    Read Oparation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/firestore';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { useEffect, useState } from 'react';
 
 function App() {
-  firebase.initializeApp({
+  const [users, setUsers] = useState([]);
+  const firebase = initializeApp({
     apiKey: "AIzaSyCMudHWoNJ5UhGOquZb14mn52L0z5tF5JE",
     authDomain: "e-man-e399f.firebaseapp.com",
     databaseURL: "https://e-man-e399f-default-rtdb.firebaseio.com",
@@ -11,11 +15,32 @@ function App() {
     messagingSenderId: "861890681843",
     appId: "1:861890681843:web:89c09a19183f38bd45d086",
     measurementId: "G-600BCZW2FG"
-  })
-  const firestore = firebase.firestore();
+  });
+
+  const db = getFirestore(firebase);
+  const getUsers = async (db) => {
+    const userCol = collection(db, 'user');
+    const userSnapshot = await getDocs(userCol);
+    const userList = userSnapshot.docs.map(doc => doc.data());
+    return userList;
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      const userFromServer = await getUsers(db)
+      setUsers(userFromServer);
+    }
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <h1>E-Man</h1>
+      <ul>
+        { users && users.map((user, index) => (
+          <li key={ index }>{ user.user_name }</li>
+        )) }
+      </ul>
     </div>
   );
 }
